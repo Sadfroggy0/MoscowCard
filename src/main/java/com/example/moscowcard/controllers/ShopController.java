4eec5bff7ab58e7d79a766acc1c0b75df76e8457
Тимофей Панов <m2005280@edu.misis.ru>
2022-05-30 01:19:48 +0300

    something...

--- src/main/java/com/example/moscowcard/controllers/ShopController.java
+++ src/main/java/com/example/moscowcard/controllers/ShopController.java
@@ -1,13 +1,10 @@
 package com.example.moscowcard.controllers;
 
 import com.example.moscowcard.services.ShopService;
-import org.hibernate.annotations.Cache;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/catalog")
@@ -21,4 +18,5 @@ public class ShopController {
         model.addAttribute("char", shopService.findAllCharacteristicsById(id));
         return "items/item";
     }
+
 }
